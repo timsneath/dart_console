595fb2f80b839ebfc82e2bc9da957029bcd0e9da
Tim Sneath <timsneath@google.com>
2019-08-05 19:06:23 -0700

    Initial ioctl implementation

--- lib/src/ffi/termlib.dart
+++ lib/src/ffi/termlib.dart
@@ -1,11 +1,31 @@
-import 'dart:ffi' as ffi;
-import 'dart:io' show Platform;
+import 'dart:ffi';
 
-typedef void_void_native_t = ffi.Void Function();
-typedef int_void_native_t = ffi.Int32 Function();
+import 'dylib_utils.dart';
+
+typedef void_void_native_t = Void Function();
+typedef int_void_native_t = Int32 Function();
+typedef ioctl_native_t = Int32 Function(
+    Int32 fd, Int32 cmd, Pointer<WinSize> winsize);
+
+const STDIN_FILENO = 0;
+const TIOCGWINSZ = 0x5413;
+
+class WinSize extends Struct<WinSize> {
+  @Int8()
+  int ws_row; // rows, in characters
+
+  @Int8()
+  int ws_col; // columns, in characters
+
+  @Int8()
+  int ws_xpixel; // horizontal size, pixels
+
+  @Int8()
+  int ws_ypixel; // vertical size, pixels
+}
 
 class TermLib {
-  ffi.DynamicLibrary termlib;
+  DynamicLibrary libc;
 
   void Function() enableRawMode;
   void Function() disableRawMode;
@@ -13,29 +33,41 @@ class TermLib {
   int Function() getWindowHeight;
   int Function() getWindowWidth;
 
+  int Function(int fd, int cmd, Pointer<WinSize> ws) ioctl;
+
   TermLib() {
-    if (Platform.isLinux) {
-      termlib = ffi.DynamicLibrary.open('/mnt/c/git/dart_console/termlib.so');
-    }
-    if (Platform.isMacOS) {
-      termlib = ffi.DynamicLibrary.open(
-          '/users/timsneath/git/me/dart_console/lib/src/ffi/termlib.dylib');
-    }
+    libc = dlopenPlatformSpecific('System');
 
-    enableRawMode = termlib
-        .lookup<ffi.NativeFunction<void_void_native_t>>('enableRawMode')
-        .asFunction();
+    ioctl = libc.lookup<NativeFunction<ioctl_native_t>>('ioctl').asFunction();
 
-    disableRawMode = termlib
-        .lookup<ffi.NativeFunction<void_void_native_t>>('disableRawMode')
-        .asFunction();
+    getWindowHeight = () {
+      Pointer<WinSize> ws = Pointer.allocate();
+      ioctl(STDIN_FILENO, TIOCGWINSZ, ws);
+      final winSize = ws.load();
+      return winSize.ws_row;
+    };
 
-    getWindowHeight = termlib
-        .lookup<ffi.NativeFunction<int_void_native_t>>('getWindowHeight')
-        .asFunction();
+    getWindowWidth = () {
+      Pointer<WinSize> ws = Pointer.allocate();
+      ioctl(STDIN_FILENO, TIOCGWINSZ, ws);
+      final winSize = ws.load();
+      return winSize.ws_col;
+    };
 
-    getWindowWidth = termlib
-        .lookup<ffi.NativeFunction<int_void_native_t>>('getWindowWidth')
-        .asFunction();
+    // enableRawMode = termlib
+    //     .lookup<NativeFunction<void_void_native_t>>('enableRawMode')
+    //     .asFunction();
+
+    // disableRawMode = termlib
+    //     .lookup<NativeFunction<void_void_native_t>>('disableRawMode')
+    //     .asFunction();
+
+    // getWindowHeight = termlib
+    //     .lookup<NativeFunction<int_void_native_t>>('getWindowHeight')
+    //     .asFunction();
+
+    // getWindowWidth = termlib
+    //     .lookup<NativeFunction<int_void_native_t>>('getWindowWidth')
+    //     .asFunction();
   }
 }
