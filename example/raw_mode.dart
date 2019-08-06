7827a04773ece37df254dd45117d3b73716257a0
Tim Sneath <timsneath@google.com>
2019-08-06 10:01:39 -0700

    Fix byte lengths

---
+++ example/raw_mode.dart
@@ -0,0 +1,9 @@
+import 'package:dart_console/dart_console.dart';
+import 'package:dart_console/src/ffi/termlib.dart';
+
+main() {
+  final termlib = TermLib();
+  termlib.enableRawMode();
+  print('Here is some text.\nHere is some more text.');
+  termlib.disableRawMode();
+}
