f047e7dced657abe20d7f512a1576c3787486063
Tim Sneath <tim@sneath.org>
2021-08-09 20:51:38 -0700

    Add basic table borders

--- example/table.dart
+++ example/table.dart
@@ -2,14 +2,16 @@ import 'package:dart_console/dart_console.dart';
 
 void main() {
   final table = Table()
+    ..setBorderStyle(BorderStyle.ascii)
+    ..setBorderType(BorderType.header)
     ..addColumnDefinition(header: 'Fruit')
     ..addColumnDefinition(header: 'Qty', alignment: TextAlignment.right)
-    ..addColumnDefinition(header: 'Notes', alignment: TextAlignment.left)
+    ..addColumnDefinition(header: 'Notes')
     ..addRow(['apples', '10'])
     ..addRow(['bananas', '5'])
     ..addRow(['apricots', '7'])
-    ..addRow(['dates', '10000000000', 'a big number'])
+    ..addRow(['dates', '10000', 'a big number'])
     ..addRow(['kumquats', '59']);
 
-  table.print();
+  table.render();
 }
--- lib/src/table.dart
+++ lib/src/table.dart
@@ -3,10 +3,17 @@ import 'dart:math' show max;
 import 'enums.dart';
 import 'console.dart';
 
+enum BorderStyle { none, ascii }
+enum BorderType { outline, header, grid }
+
 class Table {
+  late Console console;
   final List<List<String>> _table = [[]];
 
   int get columns => _table[0].length;
+
+  BorderStyle borderStyle = BorderStyle.none;
+  BorderType borderType = BorderType.header;
 
   final List<TextAlignment> _columnAlignments = [];
 
@@ -35,19 +42,83 @@ class Table {
     _table.add(fullRow);
   }
 
-  void print() {
-    final console = Console();
+  void setBorderStyle(BorderStyle borderStyle) {
+    this.borderStyle = borderStyle;
+  }
 
+  void setBorderType(BorderType borderType) {
+    this.borderType = borderType;
+  }
+
+  int _calculateTableWidth() {
+    final columnWidths =
+        _maxColumnLengths.reduce((value, element) => value + element);
+
+    // Allow padding: either a single space between columns if no border, or
+    // a padded vertical marker between columns.
+    if (borderStyle == BorderStyle.none) {
+      return columnWidths + columns - 1;
+    } else {
+      if (borderType == BorderType.outline) {
+        return columnWidths + 4 + columns - 1;
+      } else {
+        return columnWidths + 4 + (3 * (columns - 1));
+      }
+    }
+  }
+
+  void _printRule(int width) {
+    if (borderStyle != BorderStyle.none) {
+      console.writeLine('-' * width);
+    }
+  }
+
+  void render() {
     final columnWidths = _maxColumnLengths;
+    var tableWidth = _calculateTableWidth();
+    print(columnWidths);
+
+    _printRule(tableWidth);
 
     // print table rows
     for (int row = 0; row < _table.length; row++) {
+      if (borderStyle != BorderStyle.none) {
+        console.write('| ');
+      }
+
       for (int column = 0; column < columns; column++) {
         console.writeAligned(_table[row][column], columnWidths[column],
             _columnAlignments[column]);
-        console.write(' '); // padding
+
+        if (column < columns - 1) {
+          if (borderStyle != BorderStyle.none &&
+              borderType != BorderType.outline) {
+            console.write(' | ');
+          } else {
+            console.write(' ');
+          }
+        }
+      }
+
+      if (borderStyle != BorderStyle.none) {
+        console.write(' |');
       }
       console.writeLine();
+
+      // Print a rule line underneath the header only
+      if (borderType == BorderType.header && row == 0) {
+        _printRule(tableWidth);
+      }
+
+      // Print a rule line after all internal rows for grid type
+      if (borderType == BorderType.grid && row != _table.length - 1) {
+        _printRule(tableWidth);
+      }
     }
+    _printRule(tableWidth);
+  }
+
+  Table() {
+    console = Console();
   }
 }
